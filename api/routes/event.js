9d907010471de371a7911913aa54f30bb18568b9
srijit patra <srijitpatra123@gmail.com>
2022-10-12 18:17:25 +0530

    added update event route and logic

--- api/routes/event.js
+++ api/routes/event.js
@@ -8,3 +8,5 @@ const eventController = require("../controllers/event");
 router.get("/events",eventController.getEvent);
 
 router.post("/events",upload.array("files"),eventController.createEvent);
+
+router.put("/events/:id",upload.array("files"),eventController.putEvent);
