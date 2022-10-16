4a350c7d4a822da92f66df51e8a050682d991bbc
Sneha Singh <78478770+Sneha247@users.noreply.github.com>
2022-10-16 20:31:42 +0530

    Added Middle Ware

---
+++ api/middleware/index.js
@@ -0,0 +1,23 @@
+let middlewareObject = {};
+
+//a middleware to check if a user is logged in or not
+middlewareObject.isNotLoggedIn = (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+};
+
+middlewareObject.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/user/signin");
+};
+
+module.exports = middlewareObject;
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
